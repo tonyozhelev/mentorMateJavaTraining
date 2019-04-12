8643beca53400a4b730e77e7de4d0415dabdac2d
tonyo.zhelev <tonyo_zhelev@abv.bg>
2019-04-12 18:08:15 +0300

    commit exercise 5

--- JavaHomework/src/test/java/lectureFirst/LectureFirstTests.java
+++ JavaHomework/src/test/java/lectureFirst/LectureFirstTests.java
@@ -39,4 +39,39 @@ public class LectureFirstTests {
 
     }
 
+
+    //tuk v lekciite ne vidqh aritmetichni operacii obqsneni i prosto si prawq po edna ot wsichki
+    @Test
+    public void test5() {
+        int var1 = 9;
+        int var2 = 4;
+
+        int sum = var1 + var2;
+        //sbora na dvete promenlivi
+        System.out.println(sum);
+
+        int mul = var1 * var2;
+        //proizvedenieto
+        System.out.println(mul);
+
+        int diff = var1-var2;
+        //razlikata
+        System.out.println(diff);
+
+        int div = var1/var2;
+        //delenieto, no tyj kato sa celi chisla rezultata syshto e cqlata chast ot delenieto zakrygleno nadolu
+        System.out.println(div);
+
+        double div2 = (double)var1/var2;
+        //ako iskame da poluchim droben rezultat trqbwa da castnem kym double, inache dori i rezultata da e double, 2te chisla kato sa int, pak se poluchava cqlo chislo
+        System.out.println(div2);
+
+        int mod = var1%var2;
+        //modulno delenie (rezultata e ostatyka ot delenieto na dwete chisla)
+        System.out.println(mod);
+
+        //poziciqta na ++(--) operatora, opredelq prioriteta na izpylnenie, ako e pred promenlivata, pyrvo se izpulnqva toj, sled tova vs drugo, ako e sled - toj se izpylnqva posleden
+        System.out.println(var1++);
+        System.out.println(++var1);
+    }
 }
