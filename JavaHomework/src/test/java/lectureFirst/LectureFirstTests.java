b70d39ee127ef9f7d7a1ef1087c86bd605215641
tonyo.zhelev <tonyo_zhelev@abv.bg>
2019-04-12 17:36:49 +0300

    commit exercises 2-4

--- JavaHomework/src/test/java/lectureFirst/LectureFirstTests.java
+++ JavaHomework/src/test/java/lectureFirst/LectureFirstTests.java
@@ -4,13 +4,39 @@ import org.testng.annotations.Test;
 
 public class LectureFirstTests {
     @Test
-    public void Test1() {
-        char firstLetter = 0x0054;
-        char secondLetter = 0x006f;
-        char thirdLetter = 0x006e;
-        char fourthLetter = 0x0079;
-        char fifthLetter = 0x006f;
-        char[] name = new char[]{firstLetter, secondLetter, thirdLetter, fourthLetter, fifthLetter};
-        System.out.print(new String(name));
+    public void test1() {
+        char firstLetterUNI = 0x0054;
+        char secondLetterUNI = 0x006f;
+        char thirdLetterUNI = 0x006e;
+        char fourthLetterUNI = 0x0079;
+        char fifthLetterUNI = 0x006f;
+        char[] name = new char[]{firstLetterUNI, secondLetterUNI, thirdLetterUNI, fourthLetterUNI, fifthLetterUNI};
+        System.out.println(new String(name));
     }
+
+    @Test
+    public void test2() {
+        //84 111 110 121 111
+        char firstLetterASCII = 84;
+        char secondLetterASCII = 111;
+        char thirdLetterASCII = 110;
+        char fourthLetterASCII = 121;
+        char fifthLetterASCII = 111;
+        char[] name = new char[]{firstLetterASCII, secondLetterASCII, thirdLetterASCII, fourthLetterASCII, fifthLetterASCII};
+        System.out.println(new String(name));
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("\\’Hello’\\ to\\\\\\ \"World\" \\’");
+    }
+
+    @Test
+    public void test4() {
+        int minNum = -32768;
+        int maxNum = 32768;
+        System.out.println(minNum + " " + maxNum);
+
+    }
+
 }
