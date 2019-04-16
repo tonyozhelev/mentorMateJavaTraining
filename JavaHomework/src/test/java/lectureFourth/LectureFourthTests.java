36a99022686c12a1f53bf92ff0c695f55f0f0847
tonyo.zhelev <tonyo_zhelev@abv.bg>
2019-04-16 17:43:16 +0300

    add lecture 4 test 1

---
+++ JavaHomework/src/test/java/lectureFourth/LectureFourthTests.java
@@ -0,0 +1,17 @@
+package lectureFourth;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class LectureFourthTests {
+    @Test
+    public void lec4test1(){
+        int[] arr = new int[]{1788, 2025, 1869, 1456, 2013, 1458, 2458, 1254, 1472, 2365,1456, 2165, 1457, 2456};
+        String[] arrString = Arrays.stream(arr).
+                            mapToObj(x->Integer.valueOf(x).toString()).
+                            toArray(String[]::new);
+        Arrays.sort(arrString);
+        System.out.println(Arrays.toString(arrString));
+    }
+}
