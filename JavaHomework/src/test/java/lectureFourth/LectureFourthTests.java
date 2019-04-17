d677a49bb7ef87685eafb864de46939dda6dfc1b
tonyo.zhelev <tonyo_zhelev@abv.bg>
2019-04-17 12:13:51 +0300

    add lecture 4 test 2

--- JavaHomework/src/test/java/lectureFourth/LectureFourthTests.java
+++ JavaHomework/src/test/java/lectureFourth/LectureFourthTests.java
@@ -14,4 +14,11 @@ public class LectureFourthTests {
         Arrays.sort(arrString);
         System.out.println(Arrays.toString(arrString));
     }
+
+    @Test
+    public void lec4test2(){
+        int[] arr = new int[]{1788, 2025, 1869, 1456, 2013, 1458, 2458, 1254, 1472, 2365,1456, 2165, 1457, 2456};
+        int sum = 0;
+        System.out.println(Arrays.stream(arr).sum());
+    }
 }
